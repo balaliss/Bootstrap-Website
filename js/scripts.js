e476de22a40a3d7f08705a1cca92121f94cc1cbe
balaliss <balalis43@gmail.com>
2023-04-07 08:24:50 -0400

    updated api token

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,14 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// Define a function to increment the visitor count
+function incrementVisitorCount() {
+  // Make an AJAX request to a PHP script that increments the count
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'increment-count.php', true);
+  xhr.send();
+}
+
+// Call the incrementVisitorCount function when the page loads
+window.onload = incrementVisitorCount;
